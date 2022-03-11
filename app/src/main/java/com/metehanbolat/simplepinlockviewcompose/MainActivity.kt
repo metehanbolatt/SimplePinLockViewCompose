d56a9c82f51c2ea88fb5e61ab6eb28d5987cafb3
Metehan BOLAT <73827610+metehanie@users.noreply.github.com>
2022-03-12 01:34:20 +0300

    App completed.

--- app/src/main/java/com/metehanbolat/simplepinlockviewcompose/MainActivity.kt
+++ app/src/main/java/com/metehanbolat/simplepinlockviewcompose/MainActivity.kt
@@ -3,14 +3,16 @@ package com.metehanbolat.simplepinlockviewcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.ExperimentalMaterialApi
 import com.metehanbolat.simplepinlockviewcompose.ui.theme.SimplePinLockViewComposeTheme
 
+@ExperimentalMaterialApi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             SimplePinLockViewComposeTheme {
-
+                PinLockScreen()
             }
         }
     }
